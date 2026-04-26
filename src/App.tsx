64dba16d3ddb3be5bc8bcb12d32026508453721f
xipholena <mogolgogol.90@gmail.com>
2026-04-26 15:08:21 +0300

    merget into 1 feature

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import './App.css';
 import './layout/styles.css';
 import Header from './layout/header/Header.tsx';
-import Search from './feature/search/components/Search.tsx';
+import Search from './feature/movies/components/Search.tsx';
 import MoviesList from './feature/movies/components/MoviesList.tsx';
 
 const App = () => {
