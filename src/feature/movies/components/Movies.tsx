00879f3dc1b65c50bff0c865a1536e66112e2b5c
xipholena <mogolgogol.90@gmail.com>
2026-04-30 14:51:18 +0300

    additional hooks

--- src/feature/movies/components/Movies.tsx
+++ src/feature/movies/components/Movies.tsx
@@ -1,35 +1,20 @@
 import MoviesList from './MoviesList.tsx';
 import Search from './Search.tsx';
-import { useMemo, useState } from 'react';
+import { useState } from 'react';
 import useGetMovies from '../hooks/useGetMovies.ts';
-
-import { useDebounceValue } from '../../../shared/hooks/useDebouncedValue.ts';
 import { DEFAULT_ADVANCED_OPTIONS } from '../../../shared/constants.ts';
+import useMovies from '../hooks/useMovies.ts';
 
 const Movies = () => {
   const [textValue, setTextValue] = useState('');
   const [autocompleteValue, setAutocompleteValue] = useState('');
   const [toggleAutocomplete, setToggleAutocomplete] = useState(false);
   const [advancedOptions, setAdvancedOptions] = useState(DEFAULT_ADVANCED_OPTIONS);
-  const debouncedTextValue = useDebounceValue(textValue, 500);
 
-  const values = useMemo(
-    () => ({
-      ...advancedOptions,
-      query: debouncedTextValue,
-    }),
-    [advancedOptions, debouncedTextValue]
-  );
+  const { values, updateOptions } = useMovies({ textValue, setAdvancedOptions, advancedOptions });
   const { data, isLoading } = useGetMovies({
     values,
   });
-  const updateOptions = (key: string, value: string | number | boolean) => {
-    setAdvancedOptions((prev) => ({
-      ...prev,
-      [key]: value,
-    }));
-  };
-
   return (
     <>
       <Search
---
+++ src/feature/movies/hooks/useMovies.ts
@@ -0,0 +1,33 @@
+import { useMemo } from 'react';
+import { useDebounceValue } from '../../../shared/hooks/useDebouncedValue.ts';
+import { AdvancedOptions } from '../../../shared/api/interfaces.ts';
+
+interface Params {
+  textValue: string;
+  setAdvancedOptions: React.Dispatch<React.SetStateAction<AdvancedOptions>>;
+  advancedOptions: AdvancedOptions;
+}
+const useMovies = ({ textValue, advancedOptions, setAdvancedOptions }: Params) => {
+  const debouncedTextValue = useDebounceValue(textValue, 500);
+
+  const values = useMemo(
+    () => ({
+      ...advancedOptions,
+      query: debouncedTextValue,
+    }),
+    [advancedOptions, debouncedTextValue]
+  );
+
+  const updateOptions = (key: string, value: string | number | boolean) => {
+    setAdvancedOptions((prev) => ({
+      ...prev,
+      [key]: value,
+    }));
+  };
+  return {
+    updateOptions,
+    values,
+  };
+};
+
+export default useMovies;
