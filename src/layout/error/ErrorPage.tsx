7f453b219dfdd42801aa9db18543eb938537d0a5
xipholena <mogolgogol.90@gmail.com>
2026-05-01 15:41:21 +0300

    error boundaries

--- src/layout/error/ErrorPage.tsx
+++ src/layout/error/ErrorPage.tsx
@@ -1,18 +1,12 @@
-import type { FallbackProps } from 'react-error-boundary';
 import styles from './ErrorPage.module.css';
 
-const ErrorPage = (_props: FallbackProps) => {
+const ErrorPage = () => {
   return (
     <main className={styles.wrapper}>
       <section className={styles.card}>
         <p className={styles.code}>Error 500</p>
         <h1>Ooops... What could pissibly go wrong? </h1>
-        <p className={styles.description}>
-          We are working on it. Please come back later.
-        </p>
-        <button className={styles.button} onClick={resetErrorBoundary} type="button">
-          Спробувати знову
-        </button>
+        <p className={styles.description}>We are working on it. Please come back later.</p>
       </section>
     </main>
   );
